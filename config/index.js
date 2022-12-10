d60329d0f3419edecb566df9f30fe19cf71cdbfa
itai <itaimorag2000@gmail.com>
2022-12-10 16:41:47 +0200

    adding users

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
 var config
 
 if (process.env.NODE_ENV === 'production') {
-  // config = require('./prod')
-  config = require('./dev')
+  config = require('./prod')
+  // config = require('./dev')
 } else {
   // config = require('./prod')
   config = require('./dev')
